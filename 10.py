2bd43c0065601cd59d56842ec9cbc1af5c242dbf
tald <tal_200@hotmail.com>
2023-04-10 19:13:55 +0300

    init commit

--- 10.py
+++ 10.py
@@ -2,9 +2,9 @@ def square(a: int, b: int):
     mult = a * b
     addi = a + b
     return mult, addi
-blabla = square(2.3, 44)ggfgfd
+blabla = square(2.3, 44)
 print(f"addi is: {blabla[1]}")
-print(square(4, 5))fffffff
+print(square(4, 5))
 
 #    result = a * b
 #    return result
